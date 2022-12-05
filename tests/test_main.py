cc5d7108d2897253276c8e6de13fcbad7a051d97
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-12-05 16:43:31 +0900

    Добавлен тест 2

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,3 +10,11 @@ class TestMain(unittest.TestCase):
             f.flush()
             res = main.find_maximums(f.name)
             self.assertEqual([3, 6, 9], res)
+
+    def test_write_range(self):
+        a = -8
+        b = 9
+        filename = "test.txt"
+        main.write_range(filename, a, b)
+        with open(filename) as f:
+            self.assertEqual(list(map(str, range(a, b))), f.read().split())
