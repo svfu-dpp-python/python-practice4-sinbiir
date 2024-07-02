8242c29c7a3e3196ecc920688335cac71663a397
sinbiir <sinbiir12@gmail.com>
2024-07-02 20:09:08 +0900

    final

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 import os
 
-
-
-
-
 def find_maximums(path):
     arrow = []
     with open(f"{path}", "r") as f:
-    
         for i in f.readlines():
             arrow.append(max(map(int,input().split(" "))))
-        
     return arrow
 
 def write_range(name, a, b):
     with open(f"{name}", "w") as f:
         for i in range(a,b):
-            f.write(str(i))
-
-
+            f.write(str(i)+"\n")
 
 def count_files(directory_path):
-    files_list = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+    files_list = [f for f in os.listdir(directory_path)]
     return len(files_list)
 
 def open_in_notepad(filename):
@@ -31,6 +23,4 @@ def open_in_notepad(filename):
     os.system(f"notepad++ {filename}")
 
 if __name__ == "__main__":
-    # ваш тестовый код
-
     open_in_notepad("example.txt")
